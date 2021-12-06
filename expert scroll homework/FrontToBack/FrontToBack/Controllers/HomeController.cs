c8785d66c6263b666b8b269fa1ad348a3ca88e22
EmiraslanMuradov <amiraslanfm@code.edu.az>
2021-12-06 09:29:08 +0300

    expert scroll hw

---
+++ expert scroll homework/FrontToBack/FrontToBack/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using FrontToBack.DataAccessLayer;
+using FrontToBack.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontToBack.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+
+        public HomeController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var slider = _dbContext.Sliders.SingleOrDefault();
+            var sliderImages = _dbContext.SliderImages.ToList();
+            var category = _dbContext.Categories.ToList();
+            var product = _dbContext.Products.Include(x => x.Category).ToList();
+            var expertText = _dbContext.ExpertTexts.SingleOrDefault();
+            var experts = _dbContext.Experts.ToList();
+
+            return View(new HomeViewModel { 
+                Slider = slider,
+                SliderImage = sliderImages,
+                Product = product,
+                Category = category,
+                ExpertText = expertText,
+                Expert = experts
+            });
+        }
+    }
+}
---
+++ expert scroll homework/FrontToBack/FrontToBack/Startup.cs
@@ -0,0 +1,49 @@
+using FrontToBack.DataAccessLayer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontToBack
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connetctionString = _configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(connetctionString);
+            });
+
+            services.AddMvc().AddNewtonsoftJson(options => 
+            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+            app.UseStaticFiles();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{Controller=Home}/{Action=Index}/{Id?}");
+            });
+        }
+    }
+}
